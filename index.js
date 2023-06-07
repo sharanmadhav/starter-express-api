c85ba47598b72296b9a1b5b6c1a8f1ef53656686
Sharan <51872958+sharanmadhav@users.noreply.github.com>
2023-06-07 12:47:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
 app.all('/', (req, res) => {
     const requestBody = req.body;
     console.log("requestBody",requestBody);
