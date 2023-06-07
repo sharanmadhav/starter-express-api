2621bfad91c1ef834675584679080b3cb54221e2
Sharan <51872958+sharanmadhav@users.noreply.github.com>
2023-06-07 13:13:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const app = express()
 app.use(express.json());
 app.all('/', (req, res) => {
     const requestBody = req.body;
-    console.log("requestBody",requestBody);
-  res.send(requestBody);
+    const authKey =requestBody.authKey;
+    const keyList=["ABCD-1234-EFGH-5678",
+            "JKLM-5678-NOPQ-9012",
+            "UVWX-9012-YZAB-3456",
+            "CDEF-3456-GHIJ-7890",
+            "PQRS-7890-TUVW-1234",
+            "XYZA-1234-BCDE-5678",
+            "HIJK-5678-LMNO-9012",
+            "BCDE-9012-FGHI-3456",
+            "MNOP-3456-QRST-7890",
+            "WXYZ-7890-1234-5678"];
+    if(authKey){
+        if(keyList.includes(authKey.trim()){
+           res.send(true)
+        }
+        else{
+            res.send(false);
+        }
+    }
+    else{
+        res.send(false);
+    }
 })
 app.listen(process.env.PORT || 3000)
