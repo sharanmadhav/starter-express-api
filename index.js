a3c9b9f4dfb418a392318fcbaa41d88a83289caf
Sharan <51872958+sharanmadhav@users.noreply.github.com>
2023-06-07 12:45:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     const requestBody = req.body;
+    console.log("requestBody",requestBody);
   res.send(requestBody);
 })
 app.listen(process.env.PORT || 3000)
