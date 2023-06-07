699397125dfdacbce610a3287e77c155b4ba679b
Sharan <51872958+sharanmadhav@users.noreply.github.com>
2023-06-07 12:32:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
+    console.log("Just got a request!",req)
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
